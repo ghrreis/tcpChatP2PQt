57ebed9ce58186e1ebacb21e0237f84a448dea2c
Gustavo Henrique da Rocha Reis <gustavo.reis@ifsudestemg.edu.br>
2022-08-25 10:54:17 -0300

    Adicionado passagem do IP do servidor por parâmetro

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from WinChat import *
+import sys
 
 
 def main():
+    tg = sys.argv[1] # Variável para receber o IP do servidor que contém lista de usuários on-line
     app = QApplication([])
-    window = WinChat()
+    window = WinChat(tg)
     window.show()
 
     app.exec_()
